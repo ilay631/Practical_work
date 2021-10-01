8092ea3ecfa249a52689c2f88505ee2210dbda59
ilay631 <prusskih.ilay@gmail.com>
2021-10-01 13:38:49 +0300

    Fix e2

--- Practical_work1/e2.cpp
+++ Practical_work1/e2.cpp
@@ -8,9 +8,9 @@ void findMinHeight() {
 	cout << "Input square of triangle ";
 	cin >> S;
 
-	double h1 = 2 * S / a;
+	double h1 = 2.f * S / a;
 	double b = sqrt(pow(a, 2) / 4 + pow(h1, 2));
-	double h2 = 2 * S / b;
+	double h2 = 2.f * S / b;
 
 	cout << "Min height of this triangle = " << min(h1, h2) << endl;
 }
