feb0a6aad1a45f9c4fa243ef7fcd790c3287d296
ilay631 <prusskih.ilay@gmail.com>
2021-10-06 18:03:38 +0300

    Revert "Add fix e4"
    This reverts commit c0e99c0fc091d1cf37a89e1d5cd22eae5d3850aa.

--- Practical_work1/e4.cpp
+++ Practical_work1/e4.cpp
@@ -15,33 +15,24 @@ void daysYourLive() {
 	int curMonth = st.wMonth;
 	int curYear = st.wYear;
 
-	int bDay, bMonth, bYear;
+	int bday, bmonth, byear;
 	int month[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	cout << "Input your birthday" << endl;
-	cin >> bDay >> bMonth >> bYear;
+	cin >> bday >> bmonth >> byear;
 
-	int livedDays = 0;
 
-	if (bYear != curYear || bMonth != curMonth) {
-		livedDays += month[bMonth - 1] - bDay + 1;
+	int livedDays = month[curMonth - 1] - bday;
+	for (int i = bmonth - 1; i < 12; i++) {
+		livedDays += month[i];
 	}
-
-	if (bYear != curYear) {
-		for (int i = bMonth; i < 12; i++) {
-			livedDays += month[i];
-		}
-	}
-
-	for (int i = bYear + 1; i < curYear; i++) {
-		livedDays += 365;
+	for (int i = byear; i < curYear - 1; i++) {
 		if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0)
-			livedDays++;
+			livedDays += 366;
+		else
+			livedDays += 365;
 	}
-
-	if (bMonth + 1 < curMonth) {
-		for (int i = 0; i < curMonth - 1; i++) {
-			livedDays += month[i];
-		}
+	for (int i = 0; i < curMonth - 2; i++) {
+		livedDays += month[i];
 	}
 
 	livedDays += curDay;
